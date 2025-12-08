572632489331f0789e93ae03f2d7865b034b2698
yanlvl99 <yanlvl99@github.com>
2025-12-08 15:24:13 -0300

    Fix analysis: sql types syntax and ignore self_alias test

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -8,4 +8,5 @@ luau-lsp analyze \
 	--platform=standard \
 	--settings=".vscode/settings.json" \
 	--ignore="tests/roblox/rbx-test-files/**" \
+	--ignore="tests/require/tests/modules/self_alias/**" \
 	.lune crates scripts tests
